7f40de855e5c5c976d0b0b780eda4b7eda8b5202
sully <shanesully01@gmail.com>
2015-07-16 20:26:34 +0100

    Improved install doc consistency

--- install.sh
+++ install.sh
@@ -3,7 +3,8 @@
 # Setup vim directory structure
 mkdir -p vim/.vim/autoload vim/.vim/bundle vim/.vim/colors vim/.vim/.backup vim/.vim/.swp
 
-curl -LSso vim/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim # Install pathogen
+# Install pathogen
+curl -LSso vim/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim 
 
 # Download vim plugins
 cd ~/.vim/bundle
