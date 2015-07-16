19c122d59b0209a4f8634c7bdc46d4982539353f
sully <shanesully01@gmail.com>
2015-07-16 19:17:46 +0100

    Updated install script to create vim dir struct

--- install.sh
+++ install.sh
@@ -1,3 +1,5 @@
 # !/bin/bash
 
+mkdir ~/.vim/autoload ~/.vim/bundle ~/.vim/colors ~/.vim/.backup ~/.vim/.swp
+
 curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim # Install pathogen
