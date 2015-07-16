f6864aa9ad9511fb278b4db792b6b7356ad39b6e
sully <shanesully01@gmail.com>
2015-07-16 20:13:01 +0100

    Improved comment consistency

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-# Create vim directory structure
+# Setup vim directory structure
 mkdir -p vim/.vim/autoload vim/.vim/bundle vim/.vim/colors vim/.vim/.backup vim/.vim/.swp
 
 curl -LSso vim/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim # Install pathogen
@@ -11,7 +11,7 @@ git clone https://github.com/altercation/vim-colors-solarized
 git clone https://github.com/Valloric/YouCompleteMe
 git clone https://github.com/kien/ctrlp.vim
 
-# Install YouCompleteMe completion engine with semantic support for C-family languages
+# Setup YouCompleteMe completion engine with semantic support for C-family languages
 cd YouCompleteMe;git submodule update --init --recursive >/dev/null;./install.sh --clang-completer >/dev/null
 
 # Setup vim Solarized colorscheme
