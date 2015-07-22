8c78aed810fd021f3b55caf48bef5aba36fd0331
shanesully <shanesully01@gmail.com>
2015-07-22 14:30:11 +0100

    Removed zsh conf

--- install.sh
+++ install.sh
@@ -29,7 +29,6 @@ git submodule update --init --recursive
 echo "Creating symlinks..."
 
 ln -s /Users/$USER/dotfiles/shells/bash/.bash_profile ~/.bash_profile
-ln -s /Users/$USER/dotfiles/shells/zsh/.zshrc ~/.zshrc
 ln -s /Users/$USER/dotfiles/vim/.vim ~/.vim
 ln -s /Users/$USER/dotfiles/vim/.vimrc ~/.vimrc 
 ln -s /Users/$USER/dotfiles/tmux/.tmux.conf ~/.tmux.conf
